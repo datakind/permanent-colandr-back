a574b9e79d6823657fc9476febec5fa76833b7d8
Burton DeWilde <burtdewilde@gmail.com>
2016-06-12 23:34:38 -0400

    [burton] added module for getting db connection creds from environment variable

---
+++ cipy/db/db_config.py
@@ -0,0 +1,55 @@
+import os
+
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+
+# register database schemes in URLs
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('postgresql')
+urlparse.uses_netloc.append('pgsql')
+
+
+DEFAULT_ENV = 'DATABASE_URL'
+
+
+def get_conn_creds(env_var=DEFAULT_ENV):
+    """
+    Get Postgres DB connection credentials from a specially-formatted environment
+    variable, returned as a dict in the form needed for `psycopg2` to connect.
+
+    Based heavily on https://github.com/kennethreitz/dj-database-url (Copyright 2014,
+    Kenneth Reitz. All rights reserved.) but stripped down to just Postgres and
+    just connection credentials.
+
+    Args:
+        env_var (str): name of environment variable containing connection credentials;
+            'DATABASE_URL' is the default value
+
+    Returns:
+        dict: connection credentials for Postgres DB, with keys 'dbname', 'user',
+            'password', 'host', and 'port'
+    """
+    db_url = os.environ.get(env_var, None)
+    if not db_url:
+        raise OSError('environment variable "{}" not found'.format(env_var))
+    url = urlparse.urlparse(db_url)
+
+    path = url.path[1:]
+    if '?' in path and not url.query:
+        path, query = path.split('?', 2)
+    else:
+        path, query = path, url.query
+    query = urlparse.parse_qs(query)
+
+    # handle postgres percent-encoded paths
+    hostname = url.hostname or ''
+    if '%2f' in hostname.lower():
+        hostname = hostname.replace('%2f', '/').replace('%2F', '/')
+
+    return {'dbname': urlparse.unquote(path or ''),
+            'user': urlparse.unquote(url.username or ''),
+            'password': urlparse.unquote(url.password or ''),
+            'host': hostname,
+            'port': url.port or ''}
