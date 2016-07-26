ec7ba593a8b51d42304dbc8a2de4d4df51f4544a
Sam Anzaroot <sam@samanz.com>
2016-07-25 22:11:45 -0400

    Location extraction

--- pdfestrian/src/main/scala/org/datakind/ci/pdfestrian/extraction/GetLocations.scala
+++ pdfestrian/src/main/scala/org/datakind/ci/pdfestrian/extraction/GetLocations.scala
@@ -1,10 +1,38 @@
 package org.datakind.ci.pdfestrian.extraction
 
+import java.io.{BufferedWriter, FileWriter}
+
 import cc.factorie.app.nlp.{Document, TokenSpan}
 import cc.factorie.app.nlp.ner.{BilouConllNerDomain, NoEmbeddingsConllStackedChainNer, NoEmbeddingsOntonotesStackedChainNer, StaticLexiconFeatures}
 import cc.factorie.util.ModelProvider
-import org.datakind.ci.pdfestrian.scripts.Aid
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import org.datakind.ci.pdfestrian.scripts.{Aid, Interv}
 
+import scala.io.Source
+
+case class LocationTrainData(aid : Aid, location : String, sentences : Array[PredictedLocation]) {
+  def toJson : String = {
+    LocationTrainData.mapper.writeValueAsString(this)
+  }
+}
+case class PredictedLocation(valid : Boolean, location : String, sentences : Array[LocationSentence])
+case class LocationSentence(sentence : String, percent : Double)
+
+
+object LocationTrainData {
+  val mapper = new ObjectMapper() with ScalaObjectMapper
+  mapper.registerModule(DefaultScalaModule)
+  def load(string : String) : LocationTrainData =  {
+    mapper.readValue[LocationTrainData](string)
+  }
+  def fromFile(string : String) : Array[LocationTrainData] = {
+    Source.fromFile(string).getLines().toArray.map{ l=>
+      load(l)
+    }
+  }
+}
 /**
   * Created by sameyeam on 6/29/16.
   */
@@ -44,18 +72,54 @@ object GetLocations {
 
 object GetAllLocations {
   def apply(seq : Seq[Aid]) : Seq[(Aid, Seq[String])] = {
-     seq.map{ s => (s,this(s))}
+     seq.map{ s => (s,this(s).map{_._1})}
   }
 
-  def apply(trip : Aid) : Seq[String] = {
+  def apply(trip : Aid) : Seq[(String, String, Double)] = {
     PDFToDocument.apply(trip.pdf.filename) match {
       case None => Seq()
-      case Some(d) => GetLocations(d._1).map {
-        _.tokensString(" ")
+      case Some(d) =>
+        val sentences = d._1.sentences.toSeq
+        GetLocations(d._1).map { l =>
+          val percentage = sentences.indexOf(l.sentence).toDouble/sentences.length.toDouble
+        (l.tokensString(" "), l.sentence.tokens.map{ _.string }.mkString(" "), percentage)
       }
     }
   }
 
+  def grouped(trip : Aid) : Map[String, Seq[(String, Double)]] = {
+    val locations = apply(trip)
+    locations.groupBy(_._1).map{ a => a._1 -> a._2.map{i => (i._2,i._3)}}
+  }
+
+  def normalize(line : String) : String = {
+    if (line == "Viet Nam") "vietnam"
+    else if (line.startsWith("Tanzania")) "tanzania"
+    else if (line.startsWith("Iran")) "iran"
+    else if( line.startsWith("Taiwan")) "taiwan"
+    else line.toLowerCase()
+  }
+
+  def normalizeLocations(locations : Map[String, Seq[(String, Double)]]) : Map[String, Seq[(String, Double)]] = {
+    locations.map{ l => normalize(l._1) -> l._2}
+  }
+
+
+  def trainingData(aid : Aid, locations : Map[String, Seq[(String, Double)]]) : Option[LocationTrainData] = {
+    if(aid.interv.isEmpty) return None
+    val line = aid.interv.get.Int_area
+    val location = normalize(line)
+    val normalizedLocations = normalizeLocations(locations)
+    val locSentences = normalizedLocations.map{ nl =>
+      val sentences = nl._2.map{ s =>
+        LocationSentence(s._1, s._2)
+      }.toArray
+      PredictedLocation(location == nl._1, nl._1, sentences)
+    }.toArray
+    Some(LocationTrainData(aid, location, locSentences))
+  }
+
+/*
   def main(args: Array[String]) {
     val withLocations = this(Aid.load(args.head))
     withLocations.foreach{ case(aid, locations) =>
@@ -74,4 +138,33 @@ object GetAllLocations {
       }
     }
   }
+*/
+  def main(args: Array[String]): Unit = {
+    val out = new BufferedWriter(new FileWriter("locationTraining"))
+    Aid.load(args.head).foreach{ a =>
+      trainingData(a,grouped(a)) match {
+        case Some(td) =>
+          out.write(td.toJson + "\n")
+        case _ =>
+          println("No location: " + a.bib.Title)
+      }
+    }
+    out.flush()
+    out.close()
+  }
 }
+
+object FindFalse {
+  def main(args: Array[String]): Unit = {
+    LocationTrainData.fromFile(args.head).foreach{ ltd =>
+      if(!ltd.sentences.exists(_.valid) && ltd.sentences.length > 0) {
+        println(ltd.aid.bib.Title + "\n")
+        println(ltd.location + "\n")
+        println(ltd.sentences.map{ l => "Predicted: " + l.location + "\n" + l.sentences.map{_.sentence}.mkString("\n")}.mkString("\n"))
+        println("=========")
+        println("")
+      }
+     }
+  }
+}
+
---
+++ pdfestrian/src/main/scala/org/datakind/ci/pdfestrian/extraction/LocationClassifier.scala
@@ -0,0 +1,94 @@
+package org.datakind.ci.pdfestrian.extraction
+
+
+import cc.factorie.Factorie.CategoricalVectorDomain
+import cc.factorie.app.classify.{LinearVectorClassifier, OnlineOptimizingLinearVectorClassifierTrainer, OptimizingLinearVectorClassifierTrainer}
+import cc.factorie.app.nlp.{Document, Token}
+import cc.factorie.variable._
+
+import scala.io.Source
+import scala.util.Random
+
+/**
+  * Created by sameyeam on 7/25/16.
+  */
+object LocationClassifier {
+
+  implicit val rand = new Random()
+
+  val countries = Source.fromInputStream(getClass.getResourceAsStream("/countries.txt")).getLines().toSeq
+      .map{_.toLowerCase}.toSet
+
+  object LocationLabelDomain extends CategoricalDomain[String]  {
+    this += "false"
+    this += "true"
+    this.freeze()
+  }
+
+  class LocationLabel(label : String, val feature : LocationFeatures) extends CategoricalVariable[String](label) with CategoricalLabeling[String] {
+    def domain = LocationLabelDomain
+  }
+
+  object LocationFeaturesDomain extends CategoricalVectorDomain[String]
+  class LocationFeatures() extends BinaryFeatureVectorVariable[String] {
+    def domain = LocationFeaturesDomain
+    override def skipNonCategories = true
+  }
+
+  def clean(string : String) : String = {
+    val lower = string.toLowerCase()
+    lower.filter(_.isLetterOrDigit)
+  }
+
+  def sentenceToFeature(pl : PredictedLocation) : LocationLabel = {
+    val features = new LocationFeatures()
+    for(words <- pl.sentences.flatMap(p => p.sentence.split(" ").map{clean})) {
+      if(countries.contains(words))
+        features += "COUNTRY"
+      features += words
+    }
+    features += "LENGTH=" + (pl.sentences.length * 2) % 10
+    for(sentence <- pl.sentences) {
+      val location = (sentence.percent * 10.0).floor.toInt
+      features += "LOC=" + location
+    }
+    new LocationLabel(pl.valid.toString, features)
+  }
+
+  def trainDataToFeatures(locationTrainData: LocationTrainData) : Seq[LocationLabel] = {
+     locationTrainData.sentences.map{ sentenceToFeature }
+  }
+
+  def testAccuracy(testData : Seq[LocationLabel], classifier : LinearVectorClassifier[LocationLabel, LocationFeatures]) : Double = {
+      val correct = testData.count{ td => classifier.bestLabelIndex(td) == td.target.intValue}
+      correct.toDouble/testData.length
+  }
+
+  def l2f(l : LocationLabel) = l.feature
+
+  def train(trainData : Seq[LocationLabel], testData : Seq[LocationLabel]) :
+        LinearVectorClassifier[LocationLabel, LocationFeatures] = {
+    val trainer = new OnlineOptimizingLinearVectorClassifierTrainer()
+    val classifier = trainer.train(trainData, l2f)
+    println("Train Acc: " + testAccuracy(trainData,classifier) )
+    println("Test Acc: " + testAccuracy(testData,classifier) )
+    classifier
+  }
+
+  def train(fullData : Seq[LocationLabel]) : LinearVectorClassifier[LocationLabel, LocationFeatures] = {
+    val trainSize = (fullData.length.toDouble * 0.8).toInt
+    val testSize = fullData.length - trainSize
+    val shuf = Random.shuffle(fullData)
+    val trainData = shuf.take(trainSize)
+    val testData = shuf.takeRight(testSize)
+    train(trainData, testData)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val trainData = LocationTrainData.fromFile(args.head).filter( _.sentences.exists(_.valid))
+    println("Training with: " + trainData.length)
+    val labels = trainData.flatMap(trainDataToFeatures)
+    train(labels)
+  }
+
+}
