18d3442372d975be428cf2a1195d833939aada3b
Burton DeWilde <burtdewilde@gmail.com>
2016-08-16 10:34:45 -0400

    [burton] Added users-related endpoings

--- ciapi/app.py
+++ ciapi/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 
 from ciapi.resources.citations import Citation, Citations
+from ciapi.resources.users import User, NewUser
 
 
 app = Flask(__name__)
@@ -9,6 +10,8 @@ api = Api(app)
 
 api.add_resource(Citations, '/citations')
 api.add_resource(Citation, '/citations/<int:citation_id>')
+api.add_resource(User, '/users/<int:user_id>')
+api.add_resource(NewUser, '/users/register')
 
 
 if __name__ == '__main__':
---
+++ ciapi/resources/users.py
@@ -0,0 +1,68 @@
+from flask import request
+from flask_restful import Resource
+from marshmallow import validate
+from psycopg2.extensions import AsIs
+from webargs import fields
+from webargs.flaskparser import use_kwargs  # use_args
+
+from ciapi import PGDB
+import cipy
+
+
+USERS_DDL = cipy.db.db_utils.get_ddl('users')
+
+
+class User(Resource):
+
+    @use_kwargs({
+        'user_id': fields.Int(
+            required=True, location='view_args',
+            validate=validate.Range(min=1, max=2147483647)),
+        'fields': fields.DelimitedList(
+            fields.String(), delimiter=',',
+            missing=['user_id', 'name', 'email', 'review_ids', 'owned_review_ids'])
+        })
+    def get(self, user_id, fields):
+        query = """
+            SELECT %(fields)s
+            FROM users
+            WHERE user_id = %(user_id)s
+            """
+        bindings = {'fields': AsIs(','.join(fields)),
+                    'user_id': user_id}
+        results = list(PGDB.run_query(query, bindings=bindings))
+        if not results:
+            raise Exception()
+        return results[0]
+
+
+class NewUser(Resource):
+
+    @use_kwargs({
+        'name': fields.String(
+            required=True, validate=validate.Length(min=6, max=200),
+            error_messages={'required': '`name` is required!'}),
+        'email': fields.Email(
+            required=True,
+            validate=[validate.Length(min=6, max=200), validate.Email()]),
+        'password': fields.String(required=True),
+        'test': fields.Boolean(missing=False)
+        })
+    def get(self, name, email, password, test):
+        user = {'name': name, 'email': email, 'password': password}
+        user = cipy.validation.user.User(user)
+        user.validate()
+        valid_user = user.to_primitive()
+        # if test is False:
+        #     created_user_id = list(PGDB.run_query(
+        #         USERS_DDL['templates']['create_user'],
+        #         bindings=valid_user,
+        #         act=act))[0]['user_id']
+        #     return created_user_id
+        # else:
+        # HACK HACK HACK
+        result = list(PGDB.run_query(
+            USERS_DDL['templates']['create_user'],
+            bindings=valid_user,
+            act=False))
+        return user.to_primitive()
