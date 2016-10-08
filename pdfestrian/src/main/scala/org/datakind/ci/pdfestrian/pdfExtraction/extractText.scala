ccc33c71492087d8ffaff7c1aba2c42b63a6fc2b
Sam Anzaroot <sanzaroot@dataminr.com>
2016-10-08 18:41:44 -0400

    Fix extract text, add more training data

--- pdfestrian/src/main/scala/org/datakind/ci/pdfestrian/pdfExtraction/extractText.scala
+++ pdfestrian/src/main/scala/org/datakind/ci/pdfestrian/pdfExtraction/extractText.scala
@@ -1,6 +1,6 @@
 package org.datakind.ci.pdfestrian.pdfExtraction
 
-import java.io.{File, FileInputStream}
+import java.io.{BufferedWriter, File, FileInputStream, FileWriter}
 
 import org.apache.pdfbox.pdfparser.{PDFParser, PDFStreamParser}
 import org.apache.pdfbox.pdmodel.PDDocument
@@ -13,14 +13,67 @@ import org.apache.pdfbox.tools.PDFText2HTML
 object extractText {
 
   def extractText(filename : String) : String = {
-    val stream = PDDocument.load(new File(filename))
-    val textExtractor = new PDFTextStripper
-    textExtractor.getText(stream)
+    try {
+      val stream = PDDocument.load(new File(filename))
+      val textExtractor = new PDFTextStripper
+      textExtractor.getText(stream)
+    } catch {
+      case e : Exception => ""
+    }
   }
 
   def extractHTML(filename : String) : String = {
-    val stream = PDDocument.load(new File(filename))
-    val textExtractor = new PDFText2HTML
-    textExtractor.getText(stream)
+    try {
+      val stream = PDDocument.load(new File(filename))
+      val textExtractor = new PDFText2HTML
+      textExtractor.getText(stream)
+    } catch {
+      case e : Exception => ""
+    }
+  }
+  case class ExtractTextConfig(filename : String = "", html : Boolean = false)
+  val parser = new scopt.OptionParser[ExtractTextConfig]("scopt") {
+    head("scopt", "3.x")
+
+    opt[String]('f', "filename").required().action((x, c) =>
+      c.copy(filename = x)).text("Filename of pdf")
+
+    opt[Unit]('h', "html").
+      action((x, c) => c.copy(html = true)).
+      text("flag, if on will extract html file instead of plain text")
+  }
+
+  def main(args : Array[String]) : Unit = {
+    parser.parse(args, ExtractTextConfig()) match {
+      case Some(config) =>
+        if(config.html) println(extractHTML(config.filename))
+        else println(extractText(config.filename))
+      case None =>
+        println("")
+    }
+  }
+}
+
+object extractTextDirectory {
+  def main (args: Array[String] ): Unit = {
+    val dir = new File(args.head)
+    for(file <- dir.listFiles(); if file.getAbsolutePath.endsWith(".pdf")) {
+      val out = new BufferedWriter(new FileWriter(file.getAbsolutePath + ".txt"))
+      out.write(extractText.extractText(file.getAbsolutePath))
+      out.flush()
+      out.close()
+    }
+  }
+}
+
+object extractHtmlDirectory {
+  def main (args: Array[String] ): Unit = {
+    val dir = new File(args.head)
+    for(file <- dir.listFiles(); if file.getAbsolutePath.endsWith(".pdf")) {
+      val out = new BufferedWriter(new FileWriter(file.getAbsolutePath + ".html"))
+      out.write(extractText.extractHTML(file.getAbsolutePath))
+      out.flush()
+      out.close()
+    }
   }
 }
