87dc4ab4ec9e319e6a795e804036df668c1ea849
Burton DeWilde <burtdewilde@gmail.com>
2016-11-01 22:27:01 -0400

    [burton] Added COLANDR_FLASK_CONFIG to env example

--- celery_worker.py
+++ celery_worker.py
@@ -4,7 +4,7 @@ import os
 from colandr import celery, create_app
 
 
-config_name = 'dev'  # os.getenv('COLANDR_FLASK_CONFIG') or 'default'
+config_name = os.getenv('COLANDR_FLASK_CONFIG') or 'default'
 print('app config = "{}"'.format(config_name))
 app = create_app(config_name)
 app.app_context().push()
