7f16f9289bf6b3287f495f8fe7f0ac1a23b63d14
Burton DeWilde <burtdewilde@gmail.com>
2017-01-15 16:45:57 -0500

    [burton] Fixed review progress API bug; response now includes all values always

--- colandr/api/resources/review_progress.py
+++ colandr/api/resources/review_progress.py
@@ -11,6 +11,13 @@ from ...models import db, Review, Study
 from ..errors import no_data_found, unauthorized
 from ..authentication import auth
 
+SCREENING_STATUSES = (
+    'not_screened', 'screened_once', 'conflict', 'included', 'excluded')
+USER_SCREENING_STATUSES = (
+    'pending', 'awaiting_coscreener', 'conflict', 'included', 'excluded')
+EXTRACTION_STATUSES = (
+    'not_started', 'incomplete', 'complete')
+
 
 class ReviewProgressResource(Resource):
 
@@ -45,36 +52,40 @@ class ReviewProgressResource(Resource):
                         'selection_criteria': bool(review_plan.selection_criteria),
                         'data_extraction_form': bool(review_plan.data_extraction_form),
                         }
-            response['planning'] = {key: val for key, val in progress.items()
-                                    if val is True}
+            response['planning'] = progress  # {key: val for key, val in progress.items()}
         if step in ('citation_screening', 'all'):
             if user_view is False:
                 progress = db.session.query(Study.citation_status, db.func.count(1))\
                     .filter_by(review_id=id)\
                     .group_by(Study.citation_status)\
                     .all()
+                progress = dict(progress)
+                progress = {status: progress.get(status, 0)
+                            for status in SCREENING_STATUSES}
             else:
                 query = """
                     SELECT
                         (CASE
                              WHEN citation_status IN ('included', 'excluded', 'conflict') THEN citation_status
-                             WHEN citation_status = 'not_screened' OR NOT {user_id} = ANY(user_ids) THEN 'pending'
                              WHEN citation_status = 'screened_once' AND {user_id} = ANY(user_ids) THEN 'awaiting_coscreener'
+                             WHEN citation_status = 'not_screened' OR NOT {user_id} = ANY(user_ids) THEN 'pending'
                          END) AS user_status,
                          COUNT(*)
                     FROM (SELECT studies.id, studies.citation_status, screenings.user_ids
                           FROM studies
                           LEFT JOIN (SELECT citation_id, ARRAY_AGG(user_id) AS user_ids
                                      FROM citation_screenings
-                                     WHERE review_id = {review_id}
                                      GROUP BY citation_id
                                      ) AS screenings
                           ON studies.id = screenings.citation_id
+                          WHERE review_id = {review_id}
                           ) AS t
                     GROUP BY user_status;
                     """.format(user_id=g.current_user.id, review_id=id)
-                progress = [row for row in db.engine.execute(query)]
-            response['citation_screening'] = dict(progress)
+                progress = dict(row for row in db.engine.execute(query))
+                progress = {status: progress.get(status, 0)
+                            for status in USER_SCREENING_STATUSES}
+            response['citation_screening'] = progress
         if step in ('fulltext_screening', 'all'):
             if user_view is False:
                 progress = db.session.query(Study.fulltext_status, db.func.count(1))\
@@ -82,6 +93,9 @@ class ReviewProgressResource(Resource):
                     .filter_by(citation_status='included')\
                     .group_by(Study.fulltext_status)\
                     .all()
+                progress = dict(progress)
+                progress = {status: progress.get(status, 0)
+                            for status in SCREENING_STATUSES}
             else:
                 query = """
                     SELECT
@@ -90,7 +104,7 @@ class ReviewProgressResource(Resource):
                              WHEN fulltext_status = 'not_screened' OR NOT {user_id} = ANY(user_ids) THEN 'pending'
                              WHEN fulltext_status = 'screened_once' AND {user_id} = ANY(user_ids) THEN 'awaiting_coscreener'
                          END) AS user_status,
-                         COUNT(1)
+                         COUNT(*)
                     FROM (SELECT
                               studies.id,
                               studies.citation_status,
@@ -99,22 +113,27 @@ class ReviewProgressResource(Resource):
                           FROM studies
                           LEFT JOIN (SELECT fulltext_id, ARRAY_AGG(user_id) AS user_ids
                                      FROM fulltext_screenings
-                                     WHERE review_id = {review_id}
                                      GROUP BY fulltext_id
                                      ) AS screenings
                           ON studies.id = screenings.fulltext_id
+                          WHERE review_id = {review_id}
                           ) AS t
                     WHERE citation_status = 'included'  -- this is necessary!
                     GROUP BY user_status;
                     """.format(user_id=g.current_user.id, review_id=id)
-                progress = [row for row in db.engine.execute(query)]
-            response['fulltext_screening'] = dict(progress)
+                progress = dict(row for row in db.engine.execute(query))
+                progress = {status: progress.get(status, 0)
+                            for status in USER_SCREENING_STATUSES}
+            response['fulltext_screening'] = progress
         if step in ('data_extraction', 'all'):
             progress = db.session.query(Study.data_extraction_status, db.func.count(1))\
                 .filter_by(review_id=id)\
                 .filter_by(fulltext_status='included')\
                 .group_by(Study.data_extraction_status)\
                 .all()
-            response['data_extraction'] = dict(progress)
+            progress = dict(progress)
+            progress = {status: progress.get(status, 0)
+                        for status in EXTRACTION_STATUSES}
+            response['data_extraction'] = progress
 
         return response
