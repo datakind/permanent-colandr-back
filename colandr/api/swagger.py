4b17606134e7b3f62bac268bc9129efe3cc6bfa6
Burton DeWilde <burtdewilde@gmail.com>
2017-01-20 22:16:49 -0500

    [burton] Added remaining swagger models to module

--- colandr/api/swagger.py
+++ colandr/api/swagger.py
@@ -1,6 +1,6 @@
 from flask_restplus import fields
 
-# from ..lib import constants
+from ..lib import constants
 from colandr import api_
 
 
@@ -68,4 +68,77 @@ review_plan_model = api_.model(
      'selection_criteria': fields.List(fields.Nested(review_plan_selection_criterion_model)),
      'data_extraction_form': fields.List(fields.Nested(data_extraction_form_item_model)),
      'suggested_keyterms': fields.Nested(review_plan_suggested_keyterms)}
+    )
+
+import_model = api_.model(
+    'Import',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'user_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'data_source_id': fields.Integer(required=True, min=1, max=constants.MAX_BIGINT),
+     'record_type': fields.String(required=True, enum=['citation', 'fulltext']),
+     'num_records': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'status': fields.String(enum=['not_screened', 'included', 'excluded'])}
+    )
+
+dedupe_model = api_.model(
+    'Dedupe',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'duplicate_of': fields.Integer(min=1, max=constants.MAX_BIGINT),
+     'duplicate_score': fields.Float(min=0.0, max=1.0)}
+    )
+
+screening_model = api_.model(
+    'Screening',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'user_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'citation_id': fields.Integer(min=1, max=constants.MAX_BIGINT),
+     'fulltext_id': fields.Integer(min=1, max=constants.MAX_BIGINT),
+     'status': fields.String(required=True, enum=['included', 'excluded']),
+     'exclude_reasons': fields.List(fields.String(max_length=25))}
+    )
+
+citation_model = api_.model(
+    'Citation',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'type_of_work': fields.String(max_length=25),
+     'title': fields.String(max_length=300),
+     'secondary_title': fields.String(max_length=300),
+     'abstract': fields.String,
+     'pub_year': fields.Integer(min=1, max=constants.MAX_SMALLINT),
+     'pub_month': fields.Integer(min=1, max=constants.MAX_SMALLINT),
+     'authors': fields.List(fields.String(max_length=100)),
+     'keywords': fields.List(fields.String(max_length=100)),
+     'type_of_reference': fields.String(max_length=50),
+     'journal_name': fields.String(max_length=100),
+     'volume': fields.String(max_length=20),
+     'issue_number': fields.String(max_length=20),
+     'doi': fields.String(max_length=100),
+     'issn': fields.String(max_length=20),
+     'publisher': fields.String(max_length=100),
+     'language': fields.String(max_length=50),
+     'other_fields': fields.Raw}
+    )
+
+fulltext_model = api_.model(
+    'Fulltext',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT)}
+    )
+
+extracted_item_model = api_.model(
+    'ExtractedItem',
+    {'label': fields.String(required=True, max_length=25),
+     'value': fields.Raw(required=True)}
+    )
+
+data_extraction_model = api_.model(
+    'DataExtraction',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'extracted_items': fields.List(fields.Nested(extracted_item_model))}
+    )
+
+study_model = api_.model(
+    'Study',
+    {'review_id': fields.Integer(required=True, min=1, max=constants.MAX_INT),
+     'data_source_id': fields.Integer(required=True, min=1, max=constants.MAX_BIGINT),
+     'tags': fields.List(fields.String(max_length=25))}
     )
