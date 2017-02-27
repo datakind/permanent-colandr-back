4a3865615b4b33cac83ba23594cf093233fe5ad7
Burton DeWilde <burtdewilde@gmail.com>
2017-02-27 09:57:38 -0500

    [burton] bugfix for confirm_token default max_age param

--- colandr/api/registration.py
+++ colandr/api/registration.py
@@ -7,7 +7,20 @@ def generate_confirmation_token(email):
     return serializer.dumps(email, salt=current_app.config['PASSWORD_SALT'])
 
 
-def confirm_token(token, max_age=current_app.config['CONFIRM_TOKEN_EXPIRATION']):
+def confirm_token(token, max_age='default'):
+    """
+    Args:
+        token (str)
+        max_age (int or str): If "default", value is set to current app's config
+            ``CONFIRM_TOKEN_EXPIRATION``. If false-y, no max age is used.
+
+    Returns:
+        str
+    """
+    if max_age == 'default':
+        max_age = current_app.config['CONFIRM_TOKEN_EXPIRATION']
+    elif not max_age:
+        max_age = None
     serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     try:
         email = serializer.loads(
