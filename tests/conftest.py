7405dc93fec839eb74f0253c4c23bb165c519214
Burton DeWilde <burtdewilde@gmail.com>
2023-05-27 14:21:14 -0400

    tests: Update bare-bones test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 
-from colandr import create_app, db
+from colandr.app import create_app
+from colandr.extensions import db
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
-    """Create and configure a new app instance for each test."""
+    """Create and configure a new app instance, once per test session."""
     # create the app with common test config
     app = create_app("test")
     # reset database data
@@ -19,3 +20,8 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def cli_runner(app):
+    return app.test_cli_runner()
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,6 @@
-import pytest
-
 from colandr import db
 
 
-def test_get_close_db(app):
+def test_db_connection(app):
     with app.app_context():
         db.engine.execute("SELECT 1")
