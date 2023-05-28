a7a32f3b7881da5e7502d9eb725a01c64a6666bf
Burton DeWilde <burtdewilde@gmail.com>
2023-05-28 13:25:47 -0400

    build: Fix env vars and link networks in docker

--- colandr/config.py
+++ colandr/config.py
@@ -31,8 +31,8 @@ class Config:
     APP_URL_DOMAIN = 'http://localhost:5001/api'
 
     # celery+redis config
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = os.environ.get('COLANDR_CELERY_BROKER_URL', 'redis://localhost:6379/0')
+    CELERY_RESULT_BACKEND = os.environ.get('COLANDR_CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
     CELERY_ACCEPT_CONTENT = ['json']
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_RESULT_SERIALIZER = 'json'
