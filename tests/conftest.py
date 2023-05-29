388608f553bf751c1a372f6e20429f836c56be76
Burton DeWilde <burtdewilde@gmail.com>
2023-05-28 21:43:36 -0400

    tests: Add db/session fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from colandr.app import create_app
-from colandr.extensions import db
+from colandr.extensions import db as _db
 
 
 @pytest.fixture(scope="session")
@@ -9,19 +9,36 @@ def app():
     """Create and configure a new app instance, once per test session."""
     # create the app with common test config
     app = create_app("test")
-    # reset database data
     with app.app_context():
-        db.drop_all()
-        db.create_all()
+        yield app
 
-    yield app
+
+@pytest.fixture(scope="session")
+def db(app):
+    _db.drop_all()
+    _db.create_all()
+    return _db
 
 
 @pytest.fixture
 def client(app):
-    return app.test_client()
+    with app.test_client() as c:
+        yield c
 
 
 @pytest.fixture
 def cli_runner(app):
-    return app.test_cli_runner()
+    yield app.test_cli_runner()
+
+
+@pytest.fixture
+def db_session(db):
+    """
+    Allow very fast tests by using rollbacks and nested sessions. This does
+    require that your database supports SQL savepoints, and Postgres does.
+    """
+    db.session.begin_nested()
+
+    yield db.session
+
+    db.session.rollback()
