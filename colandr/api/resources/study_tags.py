aab35c3c37cf6c424ea55a7c7401cd68fd039266
Burton DeWilde <burtdewilde@gmail.com>
2023-07-04 15:38:19 -0400

    lint: Fix random lint error

--- colandr/api/resources/study_tags.py
+++ colandr/api/resources/study_tags.py
@@ -48,7 +48,7 @@ class StudyTagsResource(Resource):
                 required=True, validate=Range(min=1, max=constants.MAX_INT)
             ),
         },
-        location="view_args"
+        location="view_args",
     )
     def get(self, review_id):
         """get all distinct tags assigned to studies"""
