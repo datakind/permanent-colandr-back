52f3d1adccd0d00e1f43994719dbef83fe897dd8
Burton DeWilde <burtdewilde@gmail.com>
2023-07-29 16:21:51 -0400

    Upgrade celery (#71)
    * build: Bump celery dep, v4.0 => v5.0
    
    * build: Fix celery cmd in docker service
    
    * build: Bump celery dep, v5.0 => v5.3
    
    * build: Bump redis dep, 4.0 => 4.5
    
    * feat: Update celery config for v5.0
    
    * refactor: Rename celery app module
    
    * lint: Fix vscode nonsense

--- make_celery.py
+++ make_celery.py
@@ -6,4 +6,4 @@ from colandr.app import create_app
 
 config_name = os.getenv("COLANDR_FLASK_CONFIG") or "default"
 app = create_app(config_name)
-celery = app.extensions["celery"]
+celery_app = app.extensions["celery"]
