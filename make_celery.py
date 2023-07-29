4e43fb7558624c79aa9e70f46b92217ccf9e6597
Burton DeWilde <burtdewilde@gmail.com>
2023-07-29 15:57:00 -0400

    refactor: Rename celery app module

--- make_celery.py
+++ make_celery.py
@@ -3,7 +3,6 @@ import os
 
 from colandr.app import create_app
 
-
 config_name = os.getenv("COLANDR_FLASK_CONFIG") or "default"
 app = create_app(config_name)
-celery = app.extensions["celery"]
+celery_app = app.extensions["celery"]
