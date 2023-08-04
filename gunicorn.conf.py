9e53b83f6b22bc00dd9832b9542939b6bb561c09
Burton DeWilde <burtdewilde@gmail.com>
2023-08-04 09:57:35 -0400

    feat: Tweak gunicorn config names

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,11 +4,11 @@ import os
 
 
 bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
-workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
-threads = int(os.getenv("COLANDR_GUNICORN_THREADS", 1))
 worker_class = "sync"
+workers = int(os.getenv("COLANDR_GUNICORN_WORKERS", multiprocessing.cpu_count() * 2))
+threads = int(os.getenv("COLANDR_GUNICORN_THREADS", 1))
 reload = bool(int(os.getenv("COLANDR_GUNICORN_RELOAD", False)))
 pidfile = "./colandr.pid"
-daemon = False  # in prod, should be True?
-accesslog = "-"  # log to stdout
-loglevel = os.getenv("COLANDR_LOG_LEVEL", "info")
+daemon = False  # TODO: in prod, should be True?
+accesslog = "-"
+loglevel = os.getenv("COLANDR_GUNICORN_LOG_LEVEL", "info")
