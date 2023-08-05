af9e73812d210bd57f06f7043f33d93f2c27e1ce
Burton DeWilde <burtdewilde@gmail.com>
2023-08-05 12:59:35 -0400

    feat: Add db-seed cli cmd to add data to db

--- colandr/cli.py
+++ colandr/cli.py
@@ -1,9 +1,12 @@
+import json
 import os
+import pathlib
 import shutil
 
 import click
 from flask import Blueprint, current_app
 
+from colandr import extensions, models
 from colandr.extensions import db, guard
 from colandr.models import User
 
@@ -20,7 +23,58 @@ def create_db():
     db.create_all()
 
 
-@bp.cli.command("reset-db")
+@bp.cli.command("db-seed")
+@click.option("--fpath", "file_path", type=pathlib.Path, required=True)
+def db_seed(file_path: pathlib.Path):
+    with file_path.resolve().open(mode="r") as f:
+        data = json.load(f)
+
+    current_app.logger.info(
+        "seeding database with %s records loaded from %s ...",
+        list(data.keys()),
+        file_path,
+    )
+    for record in data["users"]:
+        user = models.User(**record)
+        user.password = extensions.guard.hash_password(user.password)
+        db.session.add(user)
+    for record in data["reviews"]:
+        db.session.add(models.Review(**record))
+    for record in data["data_sources"]:
+        db.session.add(models.DataSource(**record))
+    for record in data["imports"]:
+        db.session.add(models.Import(**record))
+    for record in data["studies"]:
+        db.session.add(models.Study(**record))
+    for record in data["citations"]:
+        db.session.add(models.Citation(**record))
+    db.session.commit()
+    # empty review plans already created w/ review
+    for record in data["review_plans"]:
+        plan = db.session.query(models.ReviewPlan).get(record["id_"])
+        for key, val in record.items():
+            if key != "id_":
+                setattr(plan, key, val)
+    for record in data["citation_screenings"]:
+        db.session.add(models.CitationScreening(**record))
+    for record in data["fulltext_uploads"]:
+        fulltext = db.session.query(models.Fulltext).get(record["id"])
+        for key, val in record.items():
+            if key != "id":
+                setattr(fulltext, key, val)
+    for record in data["fulltext_screenings"]:
+        db.session.add(models.FulltextScreening(**record))
+    # # TODO: figure out why this doesn't work :/
+    # for record in seed_data["review_teams"]:
+    #     review = db.session.query(models.Review).get(record["id"])
+    #     user = db.session.query(models.User).get(record["user_id"])
+    #     if record["action"] == "add":
+    #         review.users.append(user)
+    #     else:
+    #         raise ValueError()
+    db.session.commit()
+
+
 def reset_db():
     """
     Drop and then create all tables in the database, clear out all uploaded
