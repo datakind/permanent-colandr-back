f387d308f4be0eed4a2b6982ceca1a34e88325b9
Burton DeWilde <burtdewilde@gmail.com>
2023-08-07 20:35:47 -0400

    feat: Update sqlalchemy usage in users api

--- colandr/api/resources/users.py
+++ colandr/api/resources/users.py
@@ -67,7 +67,7 @@ class UserResource(Resource):
             is False
         ):
             return forbidden_error(f"{current_user} forbidden to get this user")
-        user = db.session.execute(sa.select(User).filter_by(id=id)).scalar_one_or_none()
+        user = db.session.get(User, id)
         if not user:
             return not_found_error(f"<User(id={id})> not found")
         if fields and "id" not in fields:
@@ -104,7 +104,7 @@ class UserResource(Resource):
         current_user = flask_praetorian.current_user()
         if id != current_user.id:
             return forbidden_error(f"{current_user} forbidden to delete this user")
-        user = db.session.execute(sa.select(User).filter_by(id=id)).scalar_one_or_none()
+        user = db.session.get(User, id)
         if not user:
             return not_found_error(f"<User(id={id})> not found")
         db.session.delete(user)
@@ -146,7 +146,7 @@ class UserResource(Resource):
         current_user = flask_praetorian.current_user()
         if id != current_user.id:
             return forbidden_error(f"{current_user} forbidden to update this user")
-        user = db.session.execute(sa.select(User).filter_by(id=id)).scalar_one_or_none()
+        user = db.session.get(User, id)
         if not user:
             return not_found_error(f"<User(id={id})> not found")
         for key, value in args.items():
@@ -211,14 +211,16 @@ class UsersResource(Resource):
         """get user record(s) for one or more matching users"""
         current_user = flask_praetorian.current_user()
         if email:
-            user = db.session.query(User).filter_by(email=email).one_or_none()
+            user = db.session.execute(
+                sa.select(User).filter_by(email=email)
+            ).scalar_one_or_none()
             if not user:
                 return not_found_error(f'no user found with email "{email}"')
             else:
                 current_app.logger.debug("got %s", user)
                 return UserSchema().dump(user)
         elif review_id:
-            review = db.session.query(Review).get(review_id)
+            review = db.session.get(Review, review_id)
             if not review:
                 return not_found_error(f"<Review(id={review_id})> not found")
             if (
