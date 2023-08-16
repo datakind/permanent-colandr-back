4d3d44cdffa10bae8f53f006dc1d3c9e2d511933
Burton DeWilde <burtdewilde@gmail.com>
2023-08-15 22:20:50 -0400

    feat: Add redis server check to api health

--- colandr/api/health.py
+++ colandr/api/health.py
@@ -1,4 +1,6 @@
+import redis
 import sqlalchemy as sa
+from celery import current_app as current_celery_app
 from flask_restx import Namespace, Resource
 
 from ..extensions import db
@@ -15,6 +17,8 @@ ns = Namespace(
 class HealthResource(Resource):
     @ns.doc(responses={200: "api is healthy"})
     def get(self):
-        # TODO: redis.ping() ?
+        redis_conn = current_celery_app.backend.client
+        assert isinstance(redis_conn, redis.client.Redis)  # type guard
+        redis_conn.ping()
         _ = db.session.execute(sa.text("SELECT 1")).scalar()
         return "OK"
