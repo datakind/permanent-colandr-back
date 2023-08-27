4f73b9b42829fc7fbf2406797812712360aa10c9
Burton DeWilde <burtdewilde@gmail.com>
2023-08-27 15:19:39 -0400

    tests: Add test for proc texst into docs func

--- tests/lib/nlp/test_utils.py
+++ tests/lib/nlp/test_utils.py
@@ -1,7 +1,62 @@
 import pytest
 import textacy
-
 from colandr.lib.nlp import utils
+from spacy.tokens import Doc
+
+
+@pytest.mark.parametrize(
+    ["texts", "max_len", "min_prob", "fallback_lang"],
+    [
+        (
+            [
+                "This is a short -- but not too short -- example English sentence.",
+                "And this is another short example English sentence.",
+                "Esta es una frase corta de ejemplo en español.",
+            ],
+            1000,
+            0.5,
+            None,
+        ),
+        (
+            [
+                "This is a short -- but not too short -- example English sentence.",
+                "And this is another short example English sentence.",
+                "Esta es una frase corta de ejemplo en español.",
+            ],
+            100,
+            0.75,
+            "en",
+        ),
+        (
+            [
+                "This is a short -- but not too short -- example English sentence.",
+                "And this is another short example English sentence.",
+                "Esta es una frase corta de ejemplo en español.",
+            ],
+            None,
+            None,
+            None,
+        ),
+    ],
+)
+def test_process_texts_into_docs(texts, max_len, min_prob, fallback_lang):
+    docs = list(
+        utils.process_texts_into_docs(
+            texts,
+            max_len=max_len,
+            min_prob=min_prob,
+            fallback_lang=fallback_lang,
+            exclude=("parser", "ner"),
+        )
+    )
+    assert len(docs) == len(texts)
+    assert all(isinstance(doc, Doc) or doc is None for doc in docs)
+    assert any(isinstance(doc, Doc) for doc in docs)
+    # sanity-check vector value for first text only
+    spacy_lang = textacy.load_spacy_lang(
+        utils.get_lang_to_models()["en"][0], exclude=("parser", "ner")
+    )
+    assert spacy_lang(texts[0]).to_bytes() == docs[0].to_bytes()
 
 
 @pytest.mark.parametrize(
