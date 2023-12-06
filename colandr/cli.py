4bfd71b9f236a3c5d49247746e4b825268404c2a
Burton DeWilde <burtdewilde@gmail.com>
2023-12-05 22:41:26 -0500

    feat: Populate review-user-assoc via cli+seed

--- colandr/cli.py
+++ colandr/cli.py
@@ -78,32 +78,10 @@ def db_seed(file_path: pathlib.Path):
                 setattr(fulltext, key, val)
     for record in data["fulltext_screenings"]:
         db.session.add(models.FulltextScreening(**record))
-    # TODO: figure out how to make this work with user deletes
-    # for record in data["review_teams"]:
-    #     review = db.session.get(models.Review, record["id"])
-    #     user = db.session.get(models.User, record["user_id"])
-    #     if record["action"] == "add":
-    #         review.users.append(user)
-    #     else:
-    #         raise ValueError()
-    review_user_associations = [
-        models.ReviewUserAssoc(
-            db.session.get(models.Review, 1), db.session.get(models.User, 1), "owner"
-        ),
-        models.ReviewUserAssoc(
-            db.session.get(models.Review, 1), db.session.get(models.User, 2), "owner"
-        ),
-        models.ReviewUserAssoc(
-            db.session.get(models.Review, 1), db.session.get(models.User, 3), "member"
-        ),
-        models.ReviewUserAssoc(
-            db.session.get(models.Review, 2), db.session.get(models.User, 2), "owner"
-        ),
-        models.ReviewUserAssoc(
-            db.session.get(models.Review, 2), db.session.get(models.User, 3), "member"
-        ),
-    ]
-    for rua in review_user_associations:
+    for record in data["review_user_associations"]:
+        review = db.session.get(models.Review, record["review_id"])
+        user = db.session.get(models.User, record["user_id"])
+        rua = models.ReviewUserAssoc(review, user, record.get("user_role"))
         db.session.add(rua)
     db.session.commit()
 
