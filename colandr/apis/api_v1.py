53e8bc590d0dd0e011762824d39a3462cc6d178c
Burton DeWilde <burtdewilde@gmail.com>
2023-12-14 22:07:40 -0500

    feat: Allow api obj in rsrcs w/o import errors

--- colandr/apis/api_v1.py
+++ colandr/apis/api_v1.py
@@ -1,4 +1,22 @@
+# ruff: noqa: E402
 from flask_restx import Api
+
+
+api_v1 = Api(
+    version="1.0",
+    prefix="/api",  # NOTE: not using "/api/v1" here to maintain backwards compatibility
+    doc="/docs",
+    default_mediatype="application/json",
+    title="colandr",
+    description="REST API powering the colandr app",
+    authorizations={
+        "api_key": {"type": "apiKey", "in": "header", "name": "Authorization"}
+        # NOTE: below style is for OpenAPI v3, which flask-restx doesn't yet support :/
+        # "access_token": {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"},
+    },
+    security="api_key",
+)
+
 
 from .auth import ns as ns_auth
 from .errors import ns as ns_errors
@@ -22,21 +40,6 @@ from .resources.study_tags import ns as ns_study_tags
 from .resources.users import ns as ns_users
 from .swagger import ns as ns_swagger
 
-
-api_v1 = Api(
-    version="1.0",
-    prefix="/api",  # NOTE: not using "/api/v1" here to maintain backwards compatibility
-    doc="/docs",
-    default_mediatype="application/json",
-    title="colandr",
-    description="REST API powering the colandr app",
-    authorizations={
-        "api_key": {"type": "apiKey", "in": "header", "name": "Authorization"}
-        # NOTE: below style is for OpenAPI v3, which flask-restx doesn't yet support :/
-        # "access_token": {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"},
-    },
-    security="api_key",
-)
 
 api_v1.add_namespace(ns_auth)
 api_v1.add_namespace(ns_errors)
