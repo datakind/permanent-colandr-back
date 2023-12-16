91dcc9de32a816c13ae3ac37f98cba3895d651e7
Burton DeWilde <burtdewilde@gmail.com>
2023-12-16 11:21:51 -0500

    tests: Improve health test

--- tests/api/test_health.py
+++ tests/api/test_health.py
@@ -1,5 +1,6 @@
 class TestHealthResource:
-    def test_get(self, db, client):
+    def test_get(self, client):
         url = "/api/health"
         response = client.get(url)
         assert response.status_code == 200
+        assert response.json == "OK"
