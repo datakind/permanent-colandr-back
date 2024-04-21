00e835d73dd1ff05036cd71b7c632e26fd8b840c
Burton DeWilde <burtdewilde@gmail.com>
2024-04-21 14:21:26 -0400

    feat: Update fulltext endpoints for model refactor

--- colandr/apis/resources/fulltext_screenings.py
+++ colandr/apis/resources/fulltext_screenings.py
@@ -8,9 +8,9 @@ from webargs import missing
 from webargs.fields import DelimitedList
 from webargs.flaskparser import use_args, use_kwargs
 
+from ... import models
 from ...extensions import db
 from ...lib import constants
-from ...models import DataExtraction, Review, Study, User  # Fulltext, FulltextScreening
 from ...utils import assign_status
 from ..errors import bad_request_error, forbidden_error, not_found_error
 from ..schemas import ScreeningSchema
@@ -61,20 +61,21 @@ class FulltextScreeningsResource(Resource):
         """get screenings for a single fulltext by id"""
         current_user = jwtext.get_current_user()
         # check current user authorization
-        fulltext = db.session.get(Fulltext, id)
-        if not fulltext:
-            return not_found_error(f"<Fulltext(id={id})> not found")
+        study = db.session.get(models.Study, id)
+        if not study:
+            return not_found_error(f"<Study(id={id})> not found")
         if (
             current_user.is_admin is False
             and current_user.review_user_assoc.filter_by(
-                review_id=fulltext.review_id
+                review_id=study.review_id
             ).one_or_none()
             is None
         ):
             return forbidden_error(
                 f"{current_user} forbidden to get fulltext screenings for this review"
             )
-        return ScreeningSchema(many=True, only=fields).dump(fulltext.screenings)
+        screenings = study.screenings.filter_by(stage="fulltext")
+        return ScreeningSchema(many=True, only=fields).dump(screenings)
 
     @ns.doc(
         responses={
@@ -96,22 +97,24 @@ class FulltextScreeningsResource(Resource):
         """delete current app user's screening for a single fulltext by id"""
         current_user = jwtext.get_current_user()
         # check current user authorization
-        fulltext = db.session.get(Fulltext, id)
-        if not fulltext:
-            return not_found_error(f"<Fulltext(id={id})> not found")
+        study = db.session.get(models.Study, id)
+        if not study:
+            return not_found_error(f"<Study(id={id})> not found")
         if (
             current_user.review_user_assoc.filter_by(
-                review_id=fulltext.review_id
+                review_id=study.review_id
             ).one_or_none()
             is None
         ):
             return forbidden_error(
                 f"{current_user} forbidden to delete fulltext screening for this review"
             )
-        screening = fulltext.screenings.filter_by(user_id=current_user.id).one_or_none()
+        screening = study.screenings.filter_by(
+            stage="fulltext", user_id=current_user.id
+        ).one_or_none()
         if not screening:
             return forbidden_error(
-                f"{current_user} has not screened {fulltext}, so nothing to delete"
+                f"{current_user} has not screened {study}, so nothing to delete"
             )
         db.session.delete(screening)
         db.session.commit()
@@ -141,22 +144,22 @@ class FulltextScreeningsResource(Resource):
         """create a screenings for a single fulltext by id"""
         current_user = jwtext.get_current_user()
         # check current user authorization
-        fulltext = db.session.get(Fulltext, id)
-        if not fulltext:
+        study = db.session.get(models.Study, id)
+        if not study:
             return not_found_error(f"<Fulltext(id={id})> not found")
         if (
             current_user.is_admin is False
             and current_user.review_user_assoc.filter_by(
-                review_id=fulltext.review_id
+                review_id=study.review_id
             ).one_or_none()
             is None
         ):
             return forbidden_error(
                 f"{current_user} forbidden to screen fulltexts for this review"
             )
-        if fulltext.filename is None:
+        if not study.fulltext:
             return forbidden_error(
-                f"user can't screen {fulltext} without first having uploaded its content"
+                f"user can't screen {study} without first having uploaded its content"
             )
         # validate and add screening
         if args["status"] == "excluded" and not args["exclude_reasons"]:
@@ -170,12 +173,19 @@ class FulltextScreeningsResource(Resource):
                 user_id = args["user_id"]
         else:
             user_id = current_user.id
-        screening = FulltextScreening(
-            fulltext.review_id, user_id, id, args["status"], args["exclude_reasons"]
+        screening = models.Screening(
+            user_id,
+            study.review_id,
+            id,
+            "fulltext",
+            args["status"],
+            args["exclude_reasons"],
         )
-        if fulltext.screenings.filter_by(user_id=current_user.id).one_or_none():
-            return forbidden_error(f"{current_user} has already screened {fulltext}")
-        fulltext.screenings.append(screening)
+        if study.screenings.filter_by(
+            stage="fulltext", user_id=current_user.id
+        ).one_or_none():
+            return forbidden_error(f"{current_user} has already screened {study}")
+        study.screenings.append(screening)
         db.session.commit()
         current_app.logger.info("inserted %s", screening)
         return ScreeningSchema().dump(screening)
@@ -208,16 +218,16 @@ class FulltextScreeningsResource(Resource):
     def put(self, args, id):
         """modify current app user's screening of a single fulltext by id"""
         current_user = jwtext.get_current_user()
-        fulltext = db.session.get(Fulltext, id)
-        if not fulltext:
-            return not_found_error(f"<Fulltext(id={id})> not found")
+        study = db.session.get(models.Study, id)
+        if not study:
+            return not_found_error(f"<Study(id={id})> not found")
         if current_user.is_admin is True and "user_id" in args:
-            screening = fulltext.screenings.filter_by(
-                user_id=args["user_id"]
+            screening = study.screenings.filter_by(
+                stage="fulltext", user_id=args["user_id"]
             ).one_or_none()
         else:
-            screening = fulltext.screenings.filter_by(
-                user_id=current_user.id
+            screening = study.screenings.filter_by(
+                stage="fulltext", user_id=current_user.id
             ).one_or_none()
         if not screening:
             return not_found_error(f"{current_user} has not screened this fulltext")
@@ -293,26 +303,32 @@ class FulltextsScreeningsResource(Resource):
             return bad_request_error(
                 "fulltext, user, and/or review id must be specified"
             )
-        query = db.session.query(FulltextScreening)
+
+        stmt = (
+            sa.select(models.Screening)
+            if status_counts is False
+            else sa.select(models.Screening.status, db.func.count(1))
+        )
+        stmt = stmt.where(models.Screening.stage == "fulltext")
         if fulltext_id is not None:
             # check user authorization
-            fulltext = db.session.get(Fulltext, fulltext_id)
-            if not fulltext:
-                return not_found_error(f"<Fulltext(id={fulltext_id})> not found")
+            study = db.session.get(models.Study, fulltext_id)
+            if not study:
+                return not_found_error(f"<Study(id={fulltext_id})> not found")
             if (
                 current_user.is_admin is False
-                and fulltext.review.review_user_assoc.filter_by(
+                and study.review.review_user_assoc.filter_by(
                     user_id=current_user.id
                 ).one_or_none()
                 is None
             ):
                 return forbidden_error(
-                    f"{current_user} forbidden to get screenings for {fulltext}"
+                    f"{current_user} forbidden to get screenings for {study}"
                 )
-            query = query.filter_by(fulltext_id=fulltext_id)
+            stmt = stmt.where(models.Screening.study_id == fulltext_id)
         if user_id is not None:
             # check user authorization
-            user = db.session.get(User, user_id)
+            user = db.session.get(models.User, user_id)
             if not user:
                 return not_found_error(f"<User(id={user_id})> not found")
             if current_user.is_admin is False and not any(
@@ -323,10 +339,10 @@ class FulltextsScreeningsResource(Resource):
                 return forbidden_error(
                     f"{current_user} forbidden to get screenings for {user}"
                 )
-            query = query.filter_by(user_id=user_id)
+            stmt = stmt.where(models.Screening.user_id == user_id)
         if review_id is not None:
             # check user authorization
-            review = db.session.get(Review, review_id)
+            review = db.session.get(models.Review, review_id)
             if not review:
                 return not_found_error(f"<Review(id={review_id})> not found")
             if (
@@ -339,13 +355,14 @@ class FulltextsScreeningsResource(Resource):
                 return forbidden_error(
                     f"{current_user} forbidden to get screenings for {review}"
                 )
-            query = query.filter_by(review_id=review_id)
+            stmt = stmt.where(models.Screening.review_id == review_id)
+
         if status_counts is True:
-            query = query.with_entities(
-                FulltextScreening.status, db.func.count(1)
-            ).group_by(FulltextScreening.status)
-            return dict(query.all())
-        return ScreeningSchema(partial=True, many=True).dump(query.all())
+            stmt = stmt.group_by(models.Screening.status)
+            return {row.status: row.count for row in db.session.execute(stmt)}
+        else:
+            results = db.session.execute(stmt)
+            return ScreeningSchema(partial=True, many=True).dump(results)
 
     @ns.doc(
         params={
@@ -389,45 +406,46 @@ class FulltextsScreeningsResource(Resource):
         if current_user.is_admin is False:
             return forbidden_error("FulltextsScreeningsResource.post is admin-only")
         # check current user authorization
-        review = db.session.get(Review, review_id)
+        review = db.session.get(models.Review, review_id)
         if not review:
             return not_found_error(f"<Review(id={review_id})> not found")
         # bulk insert fulltext screenings
         screener_user_id = user_id or current_user.id
         screenings_to_insert = []
         for screening in args:
-            screening["review_id"] = review_id
             screening["user_id"] = screener_user_id
+            screening["review_id"] = review_id
+            screening["stage"] = "fulltext"
             screenings_to_insert.append(screening)
-        db.session.execute(sa.insert(FulltextScreening), screenings_to_insert)
+        db.session.execute(sa.insert(models.Screening), screenings_to_insert)
         db.session.commit()
         current_app.logger.info(
             "inserted %s fulltext screenings", len(screenings_to_insert)
         )
         # bulk update fulltext statuses
         num_screeners = review.num_fulltext_screening_reviewers
-        fulltext_ids = sorted(s["fulltext_id"] for s in screenings_to_insert)
-        # results = db.session.query(FulltextScreening)\
-        #     .filter(FulltextScreening.fulltext_id.in_(fulltext_ids))
+        study_ids = sorted(s["study_id"] for s in screenings_to_insert)
+        # results = db.session.query(models.Screening)\
+        #     .filter(models.Screening.study_id.in_(study_ids))
         # studies_to_update = [
         #     {'id': cid, 'fulltext_status': assign_status(list(scrns), num_screeners)}
         #     for cid, scrns in itertools.groupby(results, attrgetter('fulltext_id'))
         #     ]
         with db.engine.connect() as connection:
             query = """
-                SELECT fulltext_id, ARRAY_AGG(status)
-                FROM fulltext_screenings
-                WHERE fulltext_id IN ({fulltext_ids})
-                GROUP BY fulltext_id
-                ORDER BY fulltext_id
-                """.format(fulltext_ids=",".join(str(cid) for cid in fulltext_ids))
+                SELECT study_id, ARRAY_AGG(status)
+                FROM screenings
+                WHERE study_id IN ({study_ids})
+                GROUP BY study_id
+                ORDER BY study_id
+                """.format(study_ids=",".join(str(cid) for cid in study_ids))
             results = connection.execute(sa.text(query))
         studies_to_update = [
             {"id": row[0], "fulltext_status": assign_status(row[1], num_screeners)}
             for row in results
         ]
 
-        db.session.execute(sa.update(Study), studies_to_update)
+        db.session.execute(sa.update(models.Study), studies_to_update)
         db.session.commit()
         current_app.logger.info(
             "updated fulltext_status for %s studies", len(studies_to_update)
@@ -436,26 +454,26 @@ class FulltextsScreeningsResource(Resource):
         # normally this is done automatically, but not when we're hacking
         # and doing bulk changes to the database
         results = db.session.execute(
-            sa.select(Study.id)
+            sa.select(models.Study.id)
             .filter_by(review_id=review_id, fulltext_status="included")
-            .filter(~Study.data_extraction.has())
-            .order_by(Study.id)
+            .filter(~models.Study.data_extraction.has())
+            .order_by(models.Study.id)
         ).scalars()
         data_extractions_to_insert = [
             {"id": result, "review_id": review_id} for result in results
         ]
-        db.session.execute(sa.insert(DataExtraction), data_extractions_to_insert)
+        db.session.execute(sa.insert(models.DataExtraction), data_extractions_to_insert)
         db.session.commit()
         current_app.logger.info(
             "inserted %s data extractions", len(data_extractions_to_insert)
         )
         # now update include/exclude counts on review
         status_counts_stmt = (
-            sa.select(Study.fulltext_status, db.func.count(1))
-            .filter_by(review_id=review_id)
-            # .filter(Study.fulltext_status.in_(["included", "excluded"]))
-            .filter(Study.fulltext_status == sa.any_(["included", "excluded"]))
-            .group_by(Study.fulltext_status)
+            sa.select(models.Study.fulltext_status, db.func.count(1))
+            .filter_by(review_id=review_id, dedupe_status="not_duplicate")
+            # .filter(models.Study.fulltext_status.in_(["included", "excluded"]))
+            .filter(models.Study.fulltext_status == sa.any_(["included", "excluded"]))
+            .group_by(models.Study.fulltext_status)
         )
         status_counts: dict[str, int] = {
             row.fulltext_status: row.count
