4b96657a8d00cb3e65df7091918f9ad73d3e86b7
Burton DeWilde <burtdewilde@gmail.com>
2024-05-12 11:57:19 -0400

    feat: Allow nonexistent users for part of auth

--- tests/helpers.py
+++ tests/helpers.py
@@ -10,7 +10,6 @@ from colandr import models
 def set_current_user(user_id: int, db_session: sa_orm.scoped_session):
     orig_user = getattr(flask.g, "current_user", None)
     new_user = db_session.get(models.User, user_id)
-    assert new_user is not None
     flask.g.current_user = new_user
 
     yield new_user
