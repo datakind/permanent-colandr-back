d4d0287b7391b8f6a92041bf9469e518f02230cc
Burton DeWilde <burtdewilde@gmail.com>
2025-04-19 16:12:06 -0400

    fix: ris import no longer fail on missing dttm col

--- colandr/lib/fileio/ris.py
+++ colandr/lib/fileio/ris.py
@@ -3,6 +3,7 @@ References:
     - https://github.com/asreview/citation-file-formatting/tree/main
     - https://en.wikipedia.org/wiki/RIS_(file_format)
 """
+
 import logging
 import pathlib
 import typing as t
@@ -151,7 +152,7 @@ def _sanitize_reference(reference: dict) -> dict:
         {
             key: reference[key].strftime("%Y-%m-%d")
             for key in DTTM_KEYS
-            if key in reference
+            if reference.get(key)
         }
     )
     return reference
