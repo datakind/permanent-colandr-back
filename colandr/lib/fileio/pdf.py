0caf3e2fcd825d2148bf2e3340cf79691ca1bdbe
Burton DeWilde <burtdewilde@gmail.com>
2025-07-03 13:47:30 -0400

    fix: don't sort text in pdf parser

--- colandr/lib/fileio/pdf.py
+++ colandr/lib/fileio/pdf.py
@@ -6,5 +6,6 @@ import pymupdf
 def read(file_path: str | pathlib.Path) -> str:
     """Extract text from a PDF file and write it to a text file."""
     with pymupdf.open(str(file_path), filetype="pdf") as doc:
-        text = chr(12).join(page.get_text("text", sort=True) for page in doc.pages())
+        # despite the docs, "sort" doesn't actually do what we want, so set to False
+        text = chr(12).join(page.get_text("text", sort=False) for page in doc.pages())
     return text
