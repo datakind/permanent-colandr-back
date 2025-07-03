ce8d2bc0d5f839a35c72b7ed86bc78380e245832
Burton DeWilde <burtdewilde@gmail.com>
2025-07-03 13:23:05 -0400

    refactor: update pymupdf usage

--- colandr/lib/fileio/pdf.py
+++ colandr/lib/fileio/pdf.py
@@ -1,10 +1,10 @@
 import pathlib
 
-import fitz
+import pymupdf
 
 
 def read(file_path: str | pathlib.Path) -> str:
     """Extract text from a PDF file and write it to a text file."""
-    with fitz.open(str(file_path), filetype="pdf") as doc:
-        text = chr(12).join(page.get_text("text", sort=True) for page in doc)
+    with pymupdf.open(str(file_path), filetype="pdf") as doc:
+        text = chr(12).join(page.get_text("text", sort=True) for page in doc.pages())
     return text
