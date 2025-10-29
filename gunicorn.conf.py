796c23a7b168728d059bca13317e12e6efac7dd1
Burton DeWilde <burtdewilde@gmail.com>
2025-10-28 20:55:19 -0400

    update gunicorn cfg/usage in api service (#159)
    * fix: get gunicorn conf into docker img
    
    * build: update gunicorn config file
    
    * build: use gunicorn cfg in prod api cmd

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,12 +3,13 @@ import multiprocessing
 import os
 
 
-bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
-worker_class = "sync"
+bind = f"0.0.0.0:{os.getenv('COLANDR_GUNICORN_PORT', '5000')}"
 workers = int(os.getenv("COLANDR_GUNICORN_WORKERS", multiprocessing.cpu_count() * 2))
-threads = int(os.getenv("COLANDR_GUNICORN_THREADS", 1))
-reload = bool(int(os.getenv("COLANDR_GUNICORN_RELOAD", False)))
-pidfile = "./colandr.pid"
-daemon = False  # TODO: in prod, should be True?
-accesslog = "-"
+threads = int(os.getenv("COLANDR_GUNICORN_THREADS", "1"))
+reload = bool(int(os.getenv("COLANDR_GUNICORN_RELOAD", "0")))
 loglevel = os.getenv("COLANDR_GUNICORN_LOG_LEVEL", "info")
+accesslog = "-"  # log to stdout
+pidfile = "./colandr.pid"
+
+# TODO
+# daemon = False or True? does that depend on env?
