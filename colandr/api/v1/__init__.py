4e8c9ed85ddc1fe54815c4a00e46e8ea07cae1d6
Burton DeWilde <burtdewilde@gmail.com>
2025-11-11 09:06:05 -0500

    add fulltext locations api

--- colandr/api/v1/__init__.py
+++ colandr/api/v1/__init__.py
@@ -8,6 +8,7 @@ from .routes import (
     citations,
     dedupe,
     exports,
+    fulltext_locations,
     fulltext_screenings,
     fulltext_uploads,
     fulltexts,
@@ -34,6 +35,9 @@ def register_api_blueprints(app: af.APIFlask, url_prefix: str = "/api") -> None:
     app.register_blueprint(citations.bp, url_prefix=_join_ups(url_prefix, citations.bp))
     app.register_blueprint(dedupe.bp, url_prefix=_join_ups(url_prefix, dedupe.bp))
     app.register_blueprint(exports.bp, url_prefix=_join_ups(url_prefix, exports.bp))
+    app.register_blueprint(
+        fulltext_locations.bp, url_prefix=_join_ups(url_prefix, fulltext_locations.bp)
+    )
     app.register_blueprint(
         fulltext_screenings.bp, url_prefix=_join_ups(url_prefix, fulltext_screenings.bp)
     )
