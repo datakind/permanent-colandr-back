44976a86e826b27b4ff8d198297963799961f8e7
Burton DeWilde <burtdewilde@gmail.com>
2025-11-11 21:52:29 -0500

    switch to app v1.1 in system config

--- make_celery.py
+++ make_celery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from colandr.app import create_app_v1
+from colandr.app import create_app_v1_1
 
 
-app = create_app_v1()
+app = create_app_v1_1()
 celery_app = app.extensions["celery"]
