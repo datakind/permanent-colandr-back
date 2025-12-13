e4d67c5c750a9a1ae93ed100a64c40997dd18746
Burton DeWilde <burtdewilde@gmail.com>
2025-12-13 15:57:51 -0500

    tests: update nlp utils tests to match func

--- tests/lib/nlp/test_utils.py
+++ tests/lib/nlp/test_utils.py
@@ -1,5 +1,4 @@
 import pytest
-import textacy
 from spacy.tokens import Doc
 
 from colandr.lib.nlp import utils
@@ -79,7 +78,7 @@ def test_detect_languages(texts, exp_langs):
 
 
 @pytest.mark.parametrize(
-    ["texts", "max_len", "min_prob", "fallback_lang"],
+    ["texts", "max_len", "fallback_lang"],
     [
         (
             [
@@ -88,7 +87,6 @@ def test_detect_languages(texts, exp_langs):
                 "Esta es una frase corta de ejemplo en español.",
             ],
             1000,
-            0.5,
             None,
         ),
         (
@@ -98,7 +96,6 @@ def test_detect_languages(texts, exp_langs):
                 "Esta es una frase corta de ejemplo en español.",
             ],
             100,
-            0.75,
             "en",
         ),
         (
@@ -109,16 +106,14 @@ def test_detect_languages(texts, exp_langs):
             ],
             None,
             None,
-            None,
         ),
     ],
 )
-def test_process_texts_into_docs(texts, max_len, min_prob, fallback_lang, app):
+def test_process_texts_into_docs(texts, max_len, fallback_lang, app):
     docs = list(
         utils.process_texts_into_docs(
             texts,
             max_len=max_len,
-            min_prob=min_prob,
             fallback_lang=fallback_lang,
             exclude=("parser", "ner"),
         )
@@ -127,7 +122,7 @@ def test_process_texts_into_docs(texts, max_len, min_prob, fallback_lang, app):
     assert all(isinstance(doc, Doc) or doc is None for doc in docs)
     assert any(isinstance(doc, Doc) for doc in docs)
     # sanity-check vector value for first text only
-    spacy_lang = textacy.load_spacy_lang(
+    spacy_lang = utils.load_spacy_lang(
         utils.get_lang_to_models()["en"][0], exclude=("parser", "ner")
     )
     assert spacy_lang(texts[0]).to_bytes() == docs[0].to_bytes()
