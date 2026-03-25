9c859a3203ff44497b88acde8d930d6acbcbb240
Burton DeWilde <burtdewilde@gmail.com>
2026-03-24 21:38:43 -0400

    ensure boolean search queries correctly generate (#210)
    * tests: add cases for bool search query func
    
    * try to protect from bad keyterm synonyms data
    
    * tests: add more complicated bool query case

---
+++ tests/test_utils.py
@@ -0,0 +1,32 @@
+import pytest
+
+from colandr import utils
+
+
+@pytest.mark.parametrize(
+    ["keyterms", "exp_result"],
+    [
+        (
+            [{"term": "foo", "group": "test", "synonyms": ["bar", "bat"]}],
+            '("foo" OR "bar" OR "bat")',
+        ),
+        (
+            [
+                {"term": "foo", "group": "test1"},
+                {"term": "spam", "group": "test2", "synonyms": ["eggs"]},
+            ],
+            '"foo"\nAND\n("spam" OR "eggs")',
+        ),
+        (
+            [
+                {"term": "foo", "group": "test1"},
+                {"term": "bar", "group": "test1", "synonyms": ["bat", "baz"]},
+                {"term": "spam", "group": "test2", "synonyms": ["eggs"]},
+            ],
+            '("foo" OR ("bar" OR "bat" OR "baz"))\nAND\n("spam" OR "eggs")',
+        ),
+    ],
+)
+def test_get_boolean_search_query(keyterms, exp_result):
+    result = utils.get_boolean_search_query(keyterms)
+    assert result == exp_result
